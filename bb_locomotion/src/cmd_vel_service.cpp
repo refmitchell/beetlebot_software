5a745eaf0b031dc6f3b7edb2b3cad3d7c74f7f3f
Robert Mitchell <r.mitchell@ed.ac.uk>
2023-08-15 16:44:44 +0100

    Documented bb_locomotion and bb_launchers
    In addition, removed some old unused files from bb_locomotion and
    bb_launchers. Also cleaned up remaining code in cmd_vel_service.

--- bb_locomotion/src/cmd_vel_service.cpp
+++ bb_locomotion/src/cmd_vel_service.cpp
@@ -1,3 +1,12 @@
+/**
+   \file cmd_vel_service.cpp
+   \brief A node which links the beetlebot software to the turtlebot cmd_vel topic.
+
+   This node provides a link from the computational models in bb_computation to
+   the motors on the turtlebot. This allows computational models to output motor 
+   commands without worrying how these will be interpreted.
+*/
+
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
@@ -5,21 +14,13 @@
 #include "bb_util/velocity.h"
 #include "bb_util/locomotion_cmd.h"
 
-#define ROS_LINK_DELAY = 0.3 //Found through trial and error?
-
-// Don't judge me pls
 ros::Publisher cmd_publisher;
 
-// Lock velocities in place; we may want to lock linear velocity while
-// updating angular.
-namespace node_settings {
-  bool linear_lock = false;
-  bool angular_lock = false;
-
-  inline std::string bool2string(bool b){ return b ? "true" : "false"; }
-}
-
-//Publish a velocity command.
+/**
+   Helper function which abstracts message formation.
+   \param linear the desired linear velocity
+   \param angular the desired angular velocity
+*/
 void command_velocity(float linear, float angular){
  geometry_msgs::Twist msg;
 
@@ -34,57 +35,19 @@ void command_velocity(float linear, float angular){
   cmd_publisher.publish(msg);
 }
 
+
+/**
+   The ROS service callback.
+   \param req The request portion of the srv format
+   \param res The response portion of the srv format
+   \return true on success
+*/
 bool cmd_callback (bb_util::velocity::Request& req,
                    bb_util::velocity::Response& res
                    )
 {
-  ROS_INFO("I heard: angular - %f, linear - %f", req.linear, req.angular);
+  ROS_INFO("I heard: angular - %f, linear - %f", req.angular, req.linear);
   command_velocity(req.linear, req.angular);
-
-  // geometry_msgs::Twist msg;
-
-  // msg.linear.x = req.linear;
-  // msg.linear.y = 0.0;
-  // msg.linear.z = 0.0;
-
-  // msg.angular.x = 0.0;
-  // msg.angular.y = 0.0;
-  // msg.angular.z = req.angular;
-
-  // cmd_publisher.publish(msg);
-
-  return true;
-}
-
-bool loc_node_callback(bb_util::locomotion_cmd::Request& req,
-                       bb_util::locomotion_cmd::Response& res
-                        )
-{
-  const int opcode = req.opcode;
-
-  using namespace bb_util::loc_node_commands;
-
-  switch(opcode){
-  case driving::ALL_STOP:
-    command_velocity(0,0);
-    res.response = "Stopping all motors.";
-  case driving::FWD:
-    command_velocity(0.15, 0);
-    res.response = "Linear velocity set to: 0.15.";
-  case driving::BWD:
-    command_velocity(-0.15, 0);
-    res.response = "Linear velocity set to: -0.15.";
-  case driving::LFT:
-    command_velocity(0,-0.08);
-    res.response = "Angular velocity set to: -0.08.";
-  case driving::RGT:
-    command_velocity(0,0.08);
-    res.response = "Angular velocity set to: 0.08.";
-
-  default:
-    command_velocity(0,0);
-  }
-
 
   return true;
 }
@@ -98,9 +61,6 @@ int main(int argc, char **argv)
 
   ros::ServiceServer cmd_service =
     n.advertiseService("update_velocity", cmd_callback);
-
-  // ros::ServiceServer motor_cmd_service =
-  //   n.advertiseService("motor_cmd_priority", motor_cmd_callback);
 
   // Need a small delay to let everything link up at initialisation
   ros::Duration(0.3).sleep();
